79213f72ed981d70594f027a194ef738fd1166e5
GhaziRabeh <ghazirabeh9@gmail.com>
2026-07-31 15:52:13 +0200

    fix issue port

--- src/mail.server.ts
+++ src/mail.server.ts
@@ -1,7 +1,29 @@
 import "dotenv/config";
 
+import express from "express";
+
 import { startEmailListener } from "./mail/email.listener";
 
-console.log("Mail listener started");
+const app = express();
 
-startEmailListener();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req,res)=>{
+  res.json({
+    service:"mail-listener",
+    status:"running"
+  });
+});
+
+
+app.listen(PORT,()=>{
+  console.log(`Mail listener running on port ${PORT}`);
+});
+
+
+startEmailListener()
+.then(()=>{
+  console.log("IMAP listener started");
+})
+.catch(console.error);
--- src/worker.server.ts
+++ src/worker.server.ts
@@ -1,5 +1,22 @@
 import "dotenv/config";
 
+import express from "express";
+
 import "./workers/email.worker";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    service: "worker",
+    status: "running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Worker running on port ${PORT}`);
+});
 
 console.log("Email worker started");
